861e117547b86f20bb993df864908706f12b6314
DavidWallJ <djkindread@gmail.com>
2017-08-09 18:45:09 +0800

    finished auth flow for facebook and google

---
+++ config/prod.js
@@ -0,0 +1,10 @@
+module.exports = {
+	facebookClientID: process.env.FACEBOOK_CLIENT_ID,
+	facebookClientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+	// linkedinClientID: process.env.LINKEDIN_CLIENT_ID,
+	// linkedinClientSecret: process.env.LINKEDIN_CLIENT_SECRET,
+	googleClientID: process.env.GOOGLE_CLIENT_ID,
+	googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+	mongoURI: process.env.MONGO_URI,
+	cookieKey: process.env.COOKIE_KEY
+};
