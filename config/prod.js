2bb1be4ae3310e606097a63f4462202827e88fdf
DavidWallJ <djkindread@gmail.com>
2017-08-13 14:20:34 +0800

    Revert "heroku next"
    This reverts commit f5ef6ebfa7712ce31ad9371c3446ee63e1dfc8d8.

--- config/prod.js
+++ config/prod.js
@@ -1,8 +1,8 @@
 module.exports = {
 	facebookClientID: process.env.FACEBOOK_CLIENT_ID,
 	facebookClientSecret: process.env.FACEBOOK_CLIENT_SECRET,
-	linkedinClientID: process.env.LINKEDIN_CLIENT_ID,
-	linkedinClientSecret: process.env.LINKEDIN_CLIENT_SECRET,
+	// linkedinClientID: process.env.LINKEDIN_CLIENT_ID,
+	// linkedinClientSecret: process.env.LINKEDIN_CLIENT_SECRET,
 	googleClientID: process.env.GOOGLE_CLIENT_ID,
 	googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
 	mongoURI: process.env.MONGO_URI,
