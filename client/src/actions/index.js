4c4123db85a285b65033a5e0f9ca8476d07b0114
DavidWallJ <djkindread@gmail.com>
2017-08-13 14:20:34 +0800

    Revert "prior to db refactor"
    This reverts commit 4c7745caf35b5c6313e4974e7a9fa28a420d1752.

--- client/src/actions/index.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-export const fetchUser = () => async dispatch => {
-	const res = await axios.get('/api/current_user');
-	dispatch({ type: 'fetchUser', payload: res.data });
-};
--- services/passport.js
+++ services/passport.js
@@ -1,7 +1,11 @@
+// react app generation
+// time to go back and add linkedin
+
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const FacebookStrategy = require('passport-facebook').Strategy;
 const LinkedinStrategy = require('passport-linkedin').Strategy;
+// const passportStrategies = require('./strategyFields');
 const mongoose = require('mongoose');
 const keys = require('../config/keys');
 
@@ -46,7 +50,7 @@ passport.use(
 			const user = await new User({
 				googleId: id,
 				googleDisplayName: displayName,
-				googleEmail: email
+				googleEmail: emails
 			}).save();
 			done(null, user);
 		}
@@ -63,23 +67,24 @@ passport.use(
 			proxy: true,
 			profileFields: ['id', 'displayName', 'photos', 'email']
 		},
-		async (accessToken, refreshToken, profile, done) => {
-			// console.log('Profile info: ', profile);
+		function(accessToken, refreshToken, profile, done) {
+			// console.log('Profile: ', profile._json);
 			const { id, email } = profile._json;
 			const displayName = profile._json.name;
 
-			const existingUser = await User.findOne({ facebookId: profile.id });
-
-			if (existingUser) {
-				return done(null, existingUser);
-			}
-
-			const user = await new User({
-				facebookId: id,
-				facebookDisplayName: displayName,
-				facebookEmail: email
-			}).save();
-			done(null, user);
+			User.findOne({ facebookId: id }).then(existingUser => {
+				if (existingUser) {
+					return done(null, existingUser);
+				} else {
+					new User({
+						facebookId: id,
+						facebookDisplayName: displayName,
+						facebookEmail: email
+					})
+						.save()
+						.then(user => done(null, user));
+				}
+			});
 		}
 	)
 );
@@ -93,24 +98,25 @@ passport.use(
 			proxy: true,
 			profileFields: ['id', 'first-name', 'last-name', 'email-address']
 		},
-		async (accessToken, refreshToken, profile, done) => {
-			// console.log('Profile info: ', profile);
+		function(token, tokenSecret, profile, done) {
+			console.log('Profile: ', profile._json);
 			const { id } = profile._json;
 			const displayName = profile._json.firstName;
 			const email = profile._json.emailAddress;
 
-			const existingUser = await User.findOne({ linkedinId: profile.id });
-
-			if (existingUser) {
-				return done(null, existingUser);
-			}
-
-			const user = await new User({
-				linkedinId: id,
-				linkedinDisplayName: displayName,
-				linkedinEmail: email
-			}).save();
-			done(null, user);
+			User.findOne({ linkedinId: id }).then(existingUser => {
+				if (existingUser) {
+					return done(null, existingUser);
+				} else {
+					new User({
+						linkedinId: id,
+						linkedinDisplayName: displayName,
+						linkedinEmail: email
+					})
+						.save()
+						.then(user => done(null, user));
+				}
+			});
 		}
 	)
 );
